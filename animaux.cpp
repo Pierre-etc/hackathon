c0ebb33af4cbfd4f125e5bbf318c836c2fb92abd
RBarnier <raphael.barnier@etu.minesparis.psl.eu>
2026-01-30 12:15:43 +0100

    struct ent

--- animaux.cpp
+++ animaux.cpp
@@ -16,3 +16,43 @@ inline void Grille::manger_herbe(Mouton *m)
         m->E = m->E + 15;
     }
 }
+
+inline void Mouton::alimentation()
+{
+    E += 15;
+}
+
+inline void Loup::alimentation()
+{
+    E += 35;
+}
+
+inline void Entity::deplacement(int i)
+{
+    if (i == 0)
+    {
+        x += 1;
+    }
+    if (i == 1)
+    {
+        x -= 1;
+    }
+    if (i == 2)
+    {
+        y += 1;
+    }
+    if (i == 3)
+    {
+        y -= 1;
+    }
+}
+
+inline void Entity::reproduction()
+{
+    E -= 20;
+}
+
+inline void Entity::viellissement()
+{
+    Age+=1;
+}
