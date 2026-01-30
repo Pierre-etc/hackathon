fd2b4926d21ea4d9f413dedd373c5942686e5fbf
RBarnier <raphael.barnier@etu.minesparis.psl.eu>
2026-01-30 11:30:08 +0100

    ff

--- animaux.cpp
+++ animaux.cpp
@@ -1,15 +1,18 @@
 #include "animaux.hpp"
 
-
-void def_animaux(){
-     
+void def_animaux()
+{
 }
-int position(Entity *e){
-    return *e->x+n*(*e->y);
+int position(Entity *e)
+{
+    return e->x + n * (e->y);
 }
 
-void manger_herbe(Mouton *m){
-    if(grille_herbe[position(*m)]==1){
-        grille_herbe[position(*m)]=0;
-        *m->E=*m->E+15;}
+void manger_herbe(Mouton *m)
+{
+    if (grille_herbe[position(m)] == 1)
+    {
+        grille_herbe[position(m)] = 0;
+        m->E = m->E + 15;
+    }
 }
