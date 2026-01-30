b2ad4c82b1ff4aaafe218abfa858109bbf44a60c
Pierre <pierre.dubrez@gmail.com>
2026-01-30 11:05:23 +0100

    update

--- main.cpp
+++ main.cpp
@@ -2,3 +2,22 @@
 #include "visuel.hpp"
 #include "const.hpp"
 
+
+
+int main(){
+
+
+    SDL_Window *window = NULL;
+    SDL_Renderer *renderer = NULL;
+    if (!init(&window, &renderer))
+    {
+        return 1;
+    }
+
+
+    bool running = true;
+
+
+
+
+}
