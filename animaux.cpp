01e0261c0e5119d967c3da9a7477c1fd10223e24
Esther <estherhoudant@zaclys.net>
2026-01-30 14:18:32 +0100

    update mouton

--- animaux.cpp
+++ animaux.cpp
@@ -5,8 +5,6 @@
 #include <string.h>
 #include <time.h>
 
-
-
 void def_animaux()
 {
 }
@@ -18,9 +16,8 @@ int position(Entity *e)
 inline void Grille::manger_herbe(Mouton *m)
 {
 
-    grille_herbe[position(m)] = 0;      
+    grille_herbe[position(m)] = 0;
     m->alimentation();
-    
 }
 
 inline void Grille::manger_mouton(Mouton *m, Loup *l)
@@ -29,7 +26,6 @@ inline void Grille::manger_mouton(Mouton *m, Loup *l)
     grille_animaux[position(m)] = 0;
     delete m;
 }
-
 
 inline void Mouton::alimentation()
 {
@@ -40,12 +36,13 @@ inline void Grille::renouvellement(int grille_herbe[])
 {
     for (int i = 0; i < n * n; i++)
     {
-        if (grille_herbe[i] == 0)
+        if (grille_herbe[i] == 0 && grille_animaux[i]->type != 2)
         {
             srand(time(NULL));
             int b = rand() % 10;
             if (b == 2)
-            { grille_herbe[i]=1;
+            {
+                grille_herbe[i] = 1;
 
             } // b nombre aléatoire généré entre 0 et 9, on veut une proba de
         }
@@ -55,7 +52,6 @@ inline void Loup::alimentation()
 {
     E += 35;
 }
-
 
 inline void Entity::deplacement(int i)
 {
@@ -84,5 +80,15 @@ inline void Entity::reproduction()
 
 inline void Entity::viellissement()
 {
-    age+=1;
+    age += 1;
+}
+
+inline void Grille::deplacementf(Entity *Ent)
+{
+    Null_Entity* a;
+    grille_animaux[position(Ent)] = a;
+    srand(time(NULL));
+    int i = rand() % 4;
+    Ent->deplacement(i);
+    grille_animaux[position(Ent)] = Ent;
 }
